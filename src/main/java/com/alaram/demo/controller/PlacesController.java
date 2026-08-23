4e059eb1731b8101b4ed85f6dc2ebb8206d807de
Abhay Rao <abhay11.mitmpl2025@learner.manipal.edu>
2026-08-23 17:12:11 +0530

    Update PlacesController.java

--- src/main/java/com/alaram/demo/controller/PlacesController.java
+++ src/main/java/com/alaram/demo/controller/PlacesController.java
@@ -1,8 +1,6 @@
 package com.alaram.demo.controller;
 
 import com.alaram.demo.service.PlacesService;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,17 +17,9 @@ public class PlacesController {
         this.placesService=placesService;
     }
 
-//    @GetMapping("/search")
-//    public String searchPlaces(@RequestParam String query){
-//        return placesService.searchPlaces(query);
-//    }
-
-    @GetMapping(
-            value = "/search",
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public ResponseEntity<String> searchPlaces(@RequestParam String query) {
-        return ResponseEntity.ok(placesService.searchPlaces(query));
+    @GetMapping("/search")
+    public JsonNode searchPlaces(@RequestParam("query") String query,@RequestParam("lat") double lat,@RequestParam("lon") double lon){
+        return placesService.searchPlaces(query,lat, lon);
     }
 
 }
